f3cd7588027c725eb7b622cb2d831b99d410334d
AlanWhereAreYou <alanwhereareyou@gmail.com>
2018-03-05 20:47:37 -0500

    Refactor and add Byte support
    Added Byte versions of both display methods.
    Refactored the display methods to avoid repeating code.
    Moved the Tube array protection code (that makes sure values are between 0-10) into the coding routine so it is self protectecting.
    Refactored the coding methods to avoid repeating code.
    Added an output array parameter to the array-version of the coding method to avoid damaging the input parameter.
    Added a new sample project which includes some byte operations.

--- TM1637_6D.cpp
+++ TM1637_6D.cpp
@@ -36,6 +36,8 @@
 static int8_t TubeTab[] = {0x3f,0x06,0x5b,0x4f,
                            0x66,0x6d,0x7d,0x07,
                            0x7f,0x6f,0x00,0x40};//0~9, 10=blank digit, 11=dash/minus character
+
+static int8_t SegmentToAddressMapping[] = {3,4,5,0,1,2}; // Right to left, segments are 3,4,5,0,1,2						   
 TM1637_6D::TM1637_6D(uint8_t Clk, uint8_t Data)
 {
   Clkpin = Clk;
@@ -95,57 +97,54 @@ void TM1637_6D::stop(void)
   digitalWrite(Clkpin,HIGH);
   digitalWrite(Datapin,HIGH);
 }
+
 //display function.Write to full-screen.
 void TM1637_6D::display(int8_t DispData[], int8_t DispPointData[])
 {
-  int8_t SegData[6];
-  int8_t SegPointData[6];
-  int8_t i;
-  
-  for(i = 0;i < 6;i++)
+  int8_t SegDataMapped[6];
+  coding(DispData, DispPointData, SegDataMapped);
+  displayByte(SegDataMapped);
+}
+
+void TM1637_6D::displayByte(int8_t SegData[])
+{
+  int8_t SegDataMapped[6];
+
+  for(int8_t i=0;i<6;i++)
   {
-    if(DispData[i] > 11 || DispData[i] < 0) DispData[i] = 11;
-  }
+    SegDataMapped[SegmentToAddressMapping[i]] = SegData[i];
+  } 
   
-  SegData[0] = DispData[3];
-  SegData[1] = DispData[4];
-  SegData[2] = DispData[5];
-  SegData[3] = DispData[0];
-  SegData[4] = DispData[1];
-  SegData[5] = DispData[2];
-  
-  SegPointData[0] = DispPointData[3];
-  SegPointData[1] = DispPointData[4];
-  SegPointData[2] = DispPointData[5];
-  SegPointData[3] = DispPointData[0];
-  SegPointData[4] = DispPointData[1];
-  SegPointData[5] = DispPointData[2];
-  
-  coding(SegData, SegPointData);
   start();          //start signal sent to TM1637 from MCU
   writeByte(ADDR_AUTO);//
   stop();           //
   start();          //
   writeByte(Cmd_SetAddr);//
-  for(i=0;i < 6;i ++)
+  for(int i=0;i < 6;i ++)
   {
-    writeByte(SegData[i]);        //
+    writeByte(SegDataMapped[i]);        //
   }
   stop();           //
   start();          //
   writeByte(Cmd_DispCtrl);//
   stop();           //
 }
+
 //******************************************
 void TM1637_6D::display(uint8_t BitAddr,int8_t DispData,int8_t DispPointData)
 {
   int8_t SegData;
   SegData = coding(DispData, DispPointData);
+  displayByte(BitAddr, SegData);
+}
+
+void TM1637_6D::displayByte(uint8_t BitAddr,int8_t SegData)
+{
   start();          //start signal sent to TM1637 from MCU
   writeByte(ADDR_FIXED);//
   stop();           //
   start();          //
-  writeByte(BitAddr|0xc0);//
+  writeByte(SegmentToAddressMapping[BitAddr]|0xc0);//
   writeByte(SegData);//
   stop();            //
   start();          //
@@ -287,21 +286,25 @@ void TM1637_6D::set(uint8_t brightness,uint8_t SetData,uint8_t SetAddr)
   Cmd_DispCtrl = 0x88 + brightness;//Set the brightness and it takes effect the next time it displays.
 }
 
-void TM1637_6D::coding(int8_t DispData[], int8_t DispPointData[])
+void TM1637_6D::coding(int8_t DispData[], int8_t DispPointData[], int8_t SegDataOut[])
 {
   for(uint8_t i = 0;i < 6;i ++)
   {
-    if(DispData[i] == 0x7f)DispData[i] = 0x00 + DispPointData[i];
-    else DispData[i] = TubeTab[DispData[i]] + DispPointData[i];
+	SegDataOut[i] = coding(DispData[i], DispPointData[i]);
   }
 }
 int8_t TM1637_6D::coding(int8_t DispData, int8_t DispPointData)
 {
+  if(DispData > 11 || DispData < 0) DispData = 11;
   if(DispData == 0x7f) DispData = 0x00 + DispPointData;//The bit digital tube off
   else DispData = TubeTab[DispData] + DispPointData;
+  
   return DispData;
 }
+
+
 void TM1637_6D::bitDelay(void)
 {
 	delayMicroseconds(50);
 }
+
